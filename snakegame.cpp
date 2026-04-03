10eb06ad2e91f8b4e9059b1dbaba888262e4b672
25520460-vohoanggia <25520460@gm.uit.edu.vn>
2026-04-03 20:41:18 +0700

    Giao diện: Cài đặt hàm VeKhung để vẽ viền trò chơi

--- snakegame.cpp
+++ snakegame.cpp
@@ -23,6 +23,23 @@ void SetColor(int color)
 const int WIDTH = 50;
 const int HEIGHT = 20;
 
+void VeKhung() {
+    SetColor(11); // cyan
+
+       for (int i = 0; i <= WIDTH + 1; i++) {
+        gotoxy(i, 0); cout << char(219);
+        gotoxy(i, HEIGHT); cout << char(219);
+    }
+
+    for (int i = 0; i <= HEIGHT; i++) {    
+        gotoxy(0, i); cout << char(219);
+        gotoxy(1, i); cout << char(219);
+
+        gotoxy(WIDTH, i); cout << char(219);
+        gotoxy(WIDTH + 1, i); cout << char(219);
+    }
+}
+
 int main()
 {
     // TODO: Setup game loop
