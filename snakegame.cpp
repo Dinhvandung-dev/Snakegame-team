a2f5e76e7813b229a89514ff55e6d933adfc700b
Đinh Văn Dũng <25520351@gm.uit.edu.vn>
2026-04-03 15:42:25 +0700

    Add files via upload

---
+++ snakegame.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <conio.h>
+#include <ctime>
+
+using namespace std;
+
+void gotoxy(int x, int y)
+{
+    if (x < 0 || y < 0)
+        return;
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void SetColor(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+const int WIDTH = 50;
+const int HEIGHT = 20;
+
+int main()
+{
+    // TODO: Setup game loop
+    return 0;
+}
