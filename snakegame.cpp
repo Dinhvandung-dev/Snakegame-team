815fc450debced51097f617ed18f5c76b81abb61
Đinh Văn Dũng <25520351@gm.uit.edu.vn>
2026-04-03 20:13:09 +0700

    Add files via upload

--- snakegame.cpp
+++ snakegame.cpp
@@ -6,24 +6,6 @@
 
 using namespace std;
 
-void gotoxy(int x, int y)
-{
-    if (x < 0 || y < 0)
-        return;
-    COORD c;
-    c.X = x;
-    c.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-void SetColor(int color)
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
-
-const int WIDTH = 50;
-const int HEIGHT = 20;
-
 int main()
 {
     // TODO: Setup game loop
